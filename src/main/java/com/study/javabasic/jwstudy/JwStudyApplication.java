b8db383108f62cdc72c8f3373e8ec604d6e981b4
Seong-taeHan <kmljjn22@gmail.com>
2025-06-29 13:30:05 +0900

    init: init

---
+++ src/main/java/com/study/javabasic/jwstudy/JwStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.javabasic.jwstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/javabasic/jwstudy/JwStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.javabasic.jwstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
